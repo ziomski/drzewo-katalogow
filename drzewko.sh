3e6b25f86142176ee5430ae23923e8a0727aa2d6
Mirosław Podgórski <ziomski@gmail.com>
2011-10-23 14:51:09 +0300

    Update drzewko.sh

--- drzewko.sh
+++ drzewko.sh
@@ -1,1 +1,11 @@
-mkdir -p temp/{dom,nauka/{c,logo,pascal},praca/{dokumenty,zlecenia/{nie,}zrealizowane}}
+z=zrealizowane
+zl=zlecenia
+r=rachunki
+w=wazne-sprawy
+p=praca
+mkdir -p temp/{dom/$w,nauka/{c,logo,pascal},$p/{dokumenty,$zl/{nie,}$z}}
+cd temp/dom/$w
+touch $r.txt
+cp $r.txt ../../$p/$zl/$z
+cd ../../$p/$zl/$z
+mv $r.txt wykonano.txt
